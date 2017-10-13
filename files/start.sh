26414fe5f472215274e6aee1079d3715385ca406
Boc <radu.boc@haufe-lexware.com>
2017-10-13 11:24:23 +0300

    removed consul

--- files/start.sh
+++ files/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 nginx -g 'daemon off;' &
 echo -n $LIVE > /var/live
-consul-template -consul=$CONSUL_URL -template="/templates/default.ctmpl:/etc/nginx/nginx.conf:nginx -s reload"  
+consul-template -template="/templates/default.ctmpl:/etc/nginx/nginx.conf:nginx -s reload"  
